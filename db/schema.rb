ade2342d1a264c843de12fb3e4eb0b5a5a3a40f0
FaisalAli123 <u1457222@unimail.hud.ac.uk>
2017-04-25 10:15:08 +0100

    Added New Database And Model File Called Review

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418121501) do
+ActiveRecord::Schema.define(version: 20170425091252) do
 
   create_table "filters", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20170418121501) do
     t.string   "game_img_content_type"
     t.integer  "game_img_file_size"
     t.datetime "game_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
