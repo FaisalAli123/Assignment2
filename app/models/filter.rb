fc97cdc7c2b24537a35c7b2288786d1648c6cc26
FaisalAli123 <u1457222@unimail.hud.ac.uk>
2017-05-02 13:03:12 +0100

    Added Validation To Filter And Review Pages

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,4 +1,9 @@
 class Filter < ActiveRecord::Base
 #filter has many games
   has_many :games
+
+#validation for genres
+validates :name, :presence => true
+validates :name, :length => { :minimum => 4 }
+validates :name, :uniqueness => { :message => "Already Been Used"}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ActiveRecord::Base
   belongs_to :game
 #Reviews belongs to a user
   belongs_to :user
+
+#validation for reviews
+  validates :rating, :comment, :presence => true
+  validates :comment, :length => {:minimum => 4 }
 end
